4101f45958294b2fb27085552da69cbd8b712a53
MichaelSingh7 <mikael.1@live.co.uk>
2020-08-02 20:43:59 +0000

    Updated settings.py accounts app

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url, include
 from accounts.views import logout, login, registration, user_profile
 from accounts import url_reset
+from accounts import urls as urls_accounts
 
 
 urlpatterns = [
@@ -8,6 +9,7 @@ urlpatterns = [
     url(r'^accounts/login/$', login, name="login"),
     url(r'^accounts/register/$', registration, name="registration"),
     url(r'^accounts/profile/$', user_profile, name="profile"),
-    url(r'^password-reset/', include(url_reset))
+    url(r'^password-reset/', include(url_reset)),
+    url(r'^accounts/', include(urls_accounts))
 
 ]
